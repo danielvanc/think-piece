2ae273fadb788b0da7b5c2abdcf5432b68222b71
Dan VC <danvc@ellenmacarthurfoundation.org>
2020-02-06 10:55:22 +0000

    Create User Document

--- src/firebase.js
+++ src/firebase.js
@@ -16,6 +16,31 @@ firebase.initializeApp(firebaseConfig);
 
 window.firebase = firebase;
 
+export const createUserProfileDocument = async (user, additionalData) => {
+  if (!user) return;
+
+  // Get a reerence to the place in the database where a user profile might be.
+  const userRef = firestore.doc(`users/${user.uid}`);
+
+  // Go and fetch the document from that location.
+  const snapshot = await userRef.get();
+  if (!snapshot.exists) {
+    const { displayName, email, photoURL } = user;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        photoURL,
+        createdAt,
+        ...additionalData, 
+      })
+    } catch (error) {
+      console.error(`Error ${error}`);
+    }
+  }
+}
+
 export const firestore = firebase.firestore();
 export const auth = firebase.auth();
 export const provider = new firebase.auth.GoogleAuthProvider();
