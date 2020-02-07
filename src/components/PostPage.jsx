13bb24b840853493cc0e910ab08501e38347fcf7
danielvanc <email@danielvanc.com>
2020-02-07 14:28:51 +0000

    Use withRouter to get post info and comments

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -3,6 +3,8 @@ import Post from './Post';
 import Comments from './Comments';
 import { firestore } from '../firebase';
 import { collectIdsAndDocs } from '../utilities';
+import { withRouter } from 'react-router-dom';
+
 
 class PostPage extends Component {
   state = {
@@ -10,9 +12,52 @@ class PostPage extends Component {
     comments: []
   };
 
+  get postId() {
+    return this.props.match.params.id
+  }
+
+  get postRef() {
+    return firestore.doc(`/posts/${this.postId}`)
+  }
+
+  get commentsRef() {
+    return this.postRef.collection('comments');
+  }
+
+  unsubscribeFromPost = null;
+  unsubscribeFromComments = null;
+
+  componentDidMount() {
+    this.unsubscribeFromPost = this.postRef.onSnapshot(snapshot => {
+      const post = collectIdsAndDocs(snapshot);
+      this.setState({ post });
+    });
+    this.unsubscribeFromComments = this.commentsRef.onSnapshot(snapshot => {
+      const comments = snapshot.docs.map(collectIdsAndDocs);
+      this.setState({ comments });
+    });
+  }
+  
+  componentWillUnmount() {
+    this.unsubscribeFromPost();
+    this.unsubscribeFromComments();
+  }
+  
+
   render() {
-    return <div>Post Page!</div>;
+    const { post, comments } = this.state;
+    const { match: { params: { id } } } = this.props;
+    console.log(this.props);
+    return (
+      <section>
+        {post && <Post {...post} />}
+        <Comments
+          comments={comments}
+          
+        />
+      </section>
+    )
   }
 }
 
-export default PostPage;
+export default withRouter(PostPage);
